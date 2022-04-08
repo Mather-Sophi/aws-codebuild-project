3e1033659e2cfaf87ab81f9563c20ba087c44b65
shmick <smickeler@globeandmail.com>
2022-04-08 13:54:40 -0400

    1.7.1

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
 
 resource "aws_s3_bucket" "artifact" {
   # S3 bucket cannot be longer than 63 characters
-  bucket = lower(substr("codepipeline-${local.aws_region}-${local.account_id}-${var.name}", 0, 63))
+  bucket = lower(trimsuffix(substr("codepipeline-${local.aws_region}-${local.account_id}-${var.name}", 0, 63), "-"))
   acl    = "private"
 
   lifecycle_rule {
